d986a5a9fb9cddd130376c87728c8b35bd9d08a5
EC2 Default User <ec2-user@ip-172-31-82-203.ec2.internal>
2022-12-01 03:47:26 +0000

    DevOps Class30, team 20 scripting review session2 - fixed cat multi comments

--- team20/args2.sh
+++ team20/args2.sh
@@ -1,16 +1,15 @@
-echo "Please enter your name"
-read name
+	echo "Please enter your name"
+	#read name
 
 
 
-case $1 in
+	case $1 in
  
-Florence)
-	echo "YOu have guessed it right";;
-*)
-	echo "You guessed it wrong, try again";;
-
-esac
+	Florence)
+		echo "YOu have guessed it right";;
+	*)
+		echo "You guessed it wrong, try again";; 
+	esac
 
 
 
